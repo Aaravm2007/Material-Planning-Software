584f20fcc63de842507d27707b210f967bb572be
Aarav Mehta <mehta.aarav12345@gmail.com>
2026-07-03 22:51:53 +0530

    fix: add pip install to start-backend.bat so new packages auto-install on server start

--- start-backend.bat
+++ start-backend.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "%~dp0backend"
+pip install -r requirements.txt -q
 set ALLOWED_ORIGINS=https://rocketlithum.co.in,https://www.rocketlithum.co.in,https://material-planning-software.vercel.app
 set FRONTEND_ORIGIN=https://rocketlithum.co.in
 set DEV_MODE=false
